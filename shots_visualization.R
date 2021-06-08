72b16891083a8a644ae1813e55227f441d0a4fc1
Desmond Jeffery <desmondp@byu.edu>
2021-06-08 11:46:53 -0600

    completed visualizations

---
+++ shots_visualization.R
@@ -0,0 +1,42 @@
+install.packages('sportyR')
+library(ggplot2)
+library(sportyR)
+
+data = read.csv('la_liga_shots_per_formation.csv')
+
+# Change names of X.Coordinate and Y.coordinate to x and y respectively
+names(data)[7:8] = c('x', 'y')
+
+shots = data
+
+shots = translate(shots, translate_x = -60, translate_y = -40)
+
+# Change the formation variable to get different visualizations
+formation = '41221'
+
+form_for = shots[(shots$Formation == formation) &
+                   (shots$For_or_Against == 'For') &
+                   (shots$Outcome != 'Goal'),]
+form_for_goals = shots[(shots$Formation == formation) &
+                         (shots$For_or_Against == 'For') &
+                         (shots$Outcome == 'Goal'),]
+form_away = shots[(shots$Formation == formation) &
+                    (shots$For_or_Against == 'Against')&
+                    (shots$Outcome != 'Goal'),]
+form_away_goals = shots[(shots$Formation == formation) &
+                          (shots$For_or_Against == 'Against')&
+                          (shots$Outcome == 'Goal'),]
+
+form_away = reflect(form_away, over_y = TRUE)
+form_away_goals = reflect(form_away_goals, over_y = TRUE)
+
+
+socc_stadium = geom_soccer(league = "FIFA") + labs(title=formation)
+
+socc_stadium + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_point(data = form_for, aes(x, y), color = '#2251b8') + 
+  geom_point(data = form_for_goals, aes(x, y), color = 'Red') +
+  geom_point(data = form_away, aes(x, y), color = '#fec52e') +
+  geom_point(data = form_away_goals, aes(x, y), color = 'Red')
+
