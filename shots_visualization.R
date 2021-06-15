1d241645d093444a4eb841bb5a554684389ea3e1
desmondjeffery <55635320+desmondjeffery@users.noreply.github.com>
2021-06-15 01:31:27 -0600

    new stuff

--- shots_visualization.R
+++ shots_visualization.R
@@ -10,33 +10,38 @@ names(data)[7:8] = c('x', 'y')
 shots = data
 
 shots = translate(shots, translate_x = -60, translate_y = -40)
+formations = c('4231','352','433','442','41221','4222','41212',
+               '32212','5122',	'4141',	'4321',	'343',	'3412',
+               '32221',	'3232',	'32122','42211',	'3421',
+               '451',	'4411',	'3511',	'3142')
 
-# Change the formation variable to get different visualizations
-formation = '41221'
-
-form_for = shots[(shots$Formation == formation) &
-                   (shots$For_or_Against == 'For') &
-                   (shots$Outcome != 'Goal'),]
-form_for_goals = shots[(shots$Formation == formation) &
-                         (shots$For_or_Against == 'For') &
-                         (shots$Outcome == 'Goal'),]
-form_away = shots[(shots$Formation == formation) &
-                    (shots$For_or_Against == 'Against')&
-                    (shots$Outcome != 'Goal'),]
-form_away_goals = shots[(shots$Formation == formation) &
-                          (shots$For_or_Against == 'Against')&
-                          (shots$Outcome == 'Goal'),]
-
-form_away = reflect(form_away, over_y = TRUE)
-form_away_goals = reflect(form_away_goals, over_y = TRUE)
-
-
-socc_stadium = geom_soccer(league = "FIFA") + labs(title=formation)
-
-socc_stadium + 
-  theme(plot.title = element_text(hjust = 0.5)) +
-  geom_point(data = form_for, aes(x, y), color = '#2251b8') + 
-  geom_point(data = form_for_goals, aes(x, y), color = 'Red') +
-  geom_point(data = form_away, aes(x, y), color = '#fec52e') +
-  geom_point(data = form_away_goals, aes(x, y), color = 'Red')
-
+for(formation in formations){
+  form_for = shots[(shots$Formation == formation) &
+                     (shots$For_or_Against == 'For') &
+                     (shots$Outcome != 'Goal'),]
+  form_for_goals = shots[(shots$Formation == formation) &
+                           (shots$For_or_Against == 'For') &
+                           (shots$Outcome == 'Goal'),]
+  form_away = shots[(shots$Formation == formation) &
+                      (shots$For_or_Against == 'Against')&
+                      (shots$Outcome != 'Goal'),]
+  form_away_goals = shots[(shots$Formation == formation) &
+                            (shots$For_or_Against == 'Against')&
+                            (shots$Outcome == 'Goal'),]
+  
+  form_away = reflect(form_away, over_y = TRUE)
+  form_away_goals = reflect(form_away_goals, over_y = TRUE)
+  
+  colors <- c("Team Shots" = "green", "Team Goals" = "red", 
+              "Opponent Shots" = "blue", "Opponent Goals" = "yellow")
+  
+  socc_stadium = geom_soccer(league = "FIFA") + labs(title=formation)
+  
+  print(socc_stadium + 
+    theme(plot.title = element_text(hjust = 0.5)) +
+    geom_point(data = form_for, aes(x, y, color = "Team Shots")) +
+    geom_point(data = form_for_goals, aes(x, y, color = 'Team Goals')) +
+    geom_point(data = form_away, aes(x, y, color = 'Opponent Shots')) +
+    geom_point(data = form_away_goals, aes(x, y, color = 'Opponent Goals')))
+  
+}
