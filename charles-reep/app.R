8467cd2d76eeeb76073edef72b6452ed65be38bb
Caroline Crane <cranecar@byu.edu>
2021-06-14 21:51:31 -0600

    formations

--- charles-reep/app.R
+++ charles-reep/app.R
@@ -126,6 +126,8 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                     "My first app",
                     tabPanel("Navbar 1",
                              sidebarPanel(
+                                 selectInput("formation", label="Select Formation Below", choices = c("","343","352", "3142", "3232", "3322", "3421", "3511", "32122", "32212", "32221", "433", "442", "451", "4141", "4222", "4231", "4321", "4411", "41212", "41221", "42121", "42211", "541", "5122", "5221"), selected= NULL, multiple = F),
+                                 
                                  tags$h3("Input:"), #idk what we are going to do for inputs? maybe just have static images at this point
                                  textInput("txt1", "Given Name:", ""),
                                  #textInput("txt2", "Surname:", ""),
